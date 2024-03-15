efa36a8641fb6f060e52f04a492481b65fb4a8e0
David Miller <mtech.websites@gmail.com>
2024-03-15 10:49:55 -0500

    commented out netlify plugin becuase of netlify deployment error saying plugin failed

--- config/plugins.js
+++ config/plugins.js
@@ -14,19 +14,19 @@ module.exports = ({ env }) => ({
       },
     },
   },
-  "netlify-deployments": {
-    enabled: true,
-    config: {
-      accessToken: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN,
-      sites: [
-        {
-          name: "goshengroup",
-          id: "cf04d56d-7b42-4ce1-bed5-604d64005303",
-          buildHook:
-            "https://api.netlify.com/build_hooks/65f34ed8723a5a1e86b0b3bc",
-          branch: "main", // optional
-        },
-      ],
-    },
-  },
+  // "netlify-deployments": {
+  //   enabled: true,
+  //   config: {
+  //     accessToken: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN,
+  //     sites: [
+  //       {
+  //         name: "goshengroup",
+  //         id: "cf04d56d-7b42-4ce1-bed5-604d64005303",
+  //         buildHook:
+  //           "https://api.netlify.com/build_hooks/65f34ed8723a5a1e86b0b3bc",
+  //         branch: "main", // optional
+  //       },
+  //     ],
+  //   },
+  // },
 });
